d5bb497c837b288d1a524792611d13ce963c72da
Idan Harari <84401044+IdanHarari11@users.noreply.github.com>
2025-04-08 18:00:22 +0300

    Add TestimonialsSection component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import Header from '../components/Header';
 import HeroSection from '../components/HeroSection';
 import ServicesSection from '../components/ServicesSection';
 import BookingSection from '../components/BookingSection';
+import TestimonialsSection from '../components/TestimonialsSection';
 
 export default function HomePage() {
   return (
@@ -17,6 +18,7 @@ export default function HomePage() {
     <HeroSection />
     <ServicesSection />
     <BookingSection />
+    <TestimonialsSection />
   </main>
       
       <footer className="py-6 bg-gray-100 dark:bg-gray-800">

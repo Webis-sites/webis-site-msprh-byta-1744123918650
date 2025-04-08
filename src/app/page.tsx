fc0513eac6261e8fc51cfc95912779e3a3ead4ef
Idan Harari <84401044+IdanHarari11@users.noreply.github.com>
2025-04-08 18:02:24 +0300

    Add GallerySection component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import ServicesSection from '../components/ServicesSection';
 import BookingSection from '../components/BookingSection';
 import TestimonialsSection from '../components/TestimonialsSection';
 import AboutSection from '../components/AboutSection';
+import GallerySection from '../components/GallerySection';
 
 export default function HomePage() {
   return (
@@ -21,6 +22,7 @@ export default function HomePage() {
     <BookingSection />
     <TestimonialsSection />
     <AboutSection />
+    <GallerySection />
   </main>
       
       <footer className="py-6 bg-gray-100 dark:bg-gray-800">
